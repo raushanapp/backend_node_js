33851bb4935a90631bc35e18d238322f334003a2
Raushan Kumar <sraushan890@gmail.com>
2026-07-06 13:31:23 +0530

    Add initial styles and views for the Express application
    - Created a new CSS file for styling with basic body and link styles.
    - Added an index view with placeholders for title and caption.
    - Implemented a layout view for consistent HTML structure across pages.
    - Introduced a messages view displaying a friend's message and an image.

--- Express/controller/messages.controller.js
+++ Express/controller/messages.controller.js
@@ -1,7 +1,19 @@
+const path = require("path");
+//  / floder/files.png / jpg
+
 function getMessages(req, res) {
-  return res.send(`<ul>
-           <li> Hello World!</li >
-        </ul >`);
+  //   return res.send(`<ul>
+  //            <li> Hello World!</li >
+  //         </ul >`);
+  //   return res.sendFile(
+  //     path.join(__dirname, "..", "public", "images", "test.png"),
+  //   );
+
+  res.render("messages", {
+    title: "Messages to my Friends",
+    friends: "Elon musk",
+    // layout: "layout",
+  });
 }
 
 function postMessages(req, res) {
--- Express/index.js
+++ Express/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
+const path = require("path");
+const { engine } = require("express-handlebars");
 const app = express();
+
+// app.engine("handlebars", engine());
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "layout", // matches your existing layout.handlebars filename
+  }),
+);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
 const PORT = 3000;
 
 //  Middleware example
@@ -13,8 +26,18 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}   ${req.url} ${delat} ms`);
 });
 
+//  file or serving to the client
+app.use("/site", express.static(path.join(__dirname, "public")));
+
 // this middleware tell the express parser to parse JSON bodies
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.render("index", {
+    title: "My Friends Are Clever",
+    caption: "France \  Is Beautiful!",
+  });
+});
 
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
