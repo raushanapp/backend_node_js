18b3ba045522396e6801bc600f735e1fa748bead
Raushan Kumar <sraushan890@gmail.com>
2026-07-06 07:26:04 +0530

    feat: add middleware for request logging and enhance friend addition handling

--- Express/index.js
+++ Express/index.js
@@ -14,6 +14,32 @@ const friends = [
   },
 ];
 
+//  Middleware example
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  //  actions go here....
+  const delat = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delat} ms`);
+});
+
+//
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req?.body?.name) {
+    return res.status(400).json({ error: "missing Friend name" });
+  }
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+  friends.push(newFriend);
+  return res.status(201).json(newFriend);
+});
+
 app.get("/friends", (req, res) => {
   res.status(200).json(friends);
 });
