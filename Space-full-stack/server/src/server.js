ef6ec188ea6aa8faf2432d68b9c2c8bd56fa43d2
Raushan Kumar <sraushan890@gmail.com>
2026-07-07 23:44:09 +0530

    Add planets model, controller, and router for habitable planets data
    - Implemented planets model to load and filter habitable planets from CSV data.
    - Created planets controller to handle requests for retrieving all habitable planets.
    - Set up planets router to define the endpoint for accessing planet data.

--- Space-full-stack/server/src/server.js
+++ Space-full-stack/server/src/server.js
@@ -2,14 +2,19 @@ const http = require("http");
 
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
 
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}......`);
+  });
+}
+
+startServer();
 // app.listen()
-
-console.log(PORT);
