d64530ef678693e9989d2bdbe9aeaec56723aee9
Raushan Kumar <sraushan890@gmail.com>
2026-07-08 07:34:04 +0530

    feat: update .gitignore, modify build script, and add deployment script; integrate morgan for logging and serve static files

--- Space-full-stack/server/src/app.js
+++ Space-full-stack/server/src/app.js
@@ -1,8 +1,12 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(
   cors({
@@ -10,7 +14,15 @@ app.use(
   }),
 );
 
+// this logger will log all requests
+app.use(morgan("combined"));
+
 app.use(express.json());
+
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
---
+++ Space-full-stack/server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "kepler Exploration name",
+  rocket: "Explorer",
+  launchDate: new Date("December 17,2030"),
+  destination: "Kepler-442 b",
+  customer: ["NASA", "SpaceX", "ISRO"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
