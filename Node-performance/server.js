3049ce3fa8b22b8aa9964c3962b213e6be9bd443
Raushan Kumar <sraushan890@gmail.com>
2026-07-09 22:31:47 +0530

    Add initial server implementation with performance example routes
    - Set up an Express server listening on a specified port.
    - Implemented a root route that returns the process ID.
    - Added a '/timer' route that simulates a delay in response.
    - Included commented-out code for clustering and PM2 integration for future scalability.

---
+++ Node-performance/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+// const cluster = require("cluster");
+// const os = require("os");
+
+// this for the windows operating system and the other operating  systems handling automatically
+
+// cluster.schedulingPolicy = cluster.SCHED_RR;
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+function delay(durations) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < durations) {
+    //  event loop is blocked
+  }
+}
+
+app.get("/", (req, res) => {
+  // JSON.stringify({})=> "{}",  this take in javascript object and return the string representation
+  // JSON.parse("{}")=>{} this take a string and return the javascript object
+  // [5,4,1,2,3,4].sort()
+  //  this fucntion do not take  to much time only take few milliseconds
+  return res.send(`Performance example ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+  //  delay the response
+  delay(9000);
+  return res.send(`Ding ding ding! ${process.pid}`);
+});
+
+//  if we install pm2 tool then we don't have these code implemented and
+// pm2 automatic handle these things
+
+// console.log("Running server .js.....");
+// if (cluster.isPrimary) {
+//   console.log("Master has been started.....");
+//   const NUM_WORKERS = os.cpus().length;
+//   for (let i = 0; i < NUM_WORKERS; i++) {
+//     cluster.fork();
+//   }
+// } else {
+//   console.log(`Worker has been started with PID: ${process.pid}`);
+
+//   app.listen(PORT);
+// }
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
