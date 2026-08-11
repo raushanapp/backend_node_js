c5b688e19b7168c1cf446b801a43a4655230f131
Raushan Kumar <sraushan890@gmail.com>
2026-08-11 08:11:09 +0530

    feat: add CORS support and integrate express middleware for Apollo Server

--- Graphql-example/server.js
+++ Graphql-example/server.js
@@ -1,12 +1,17 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
+
 const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@as-integrations/express5");
 
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+
+const PORT = 4000;
 
 async function startApolloServer() {
   const app = express();
@@ -19,12 +24,14 @@ async function startApolloServer() {
   const server = new ApolloServer({ schema });
 
   await server.start();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(PORT, () => {
+    console.log(`Running Graphql server ${PORT}`);
+  });
 }
 
-const PORT = 4000;
-
-app.use("/graphql", () => {});
-
-app.listen(PORT, () => {
-  console.log(`Running Graphql server ${PORT}`);
-});
+startApolloServer();
